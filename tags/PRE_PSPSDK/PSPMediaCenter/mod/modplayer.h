41f49fa6276d2c300f25c3ecdc091c2ecdecf280
warren <warren@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-06-26 16:28:32 +0000

    Last Version before port to PSPSDK v1.0

---
+++ tags/PRE_PSPSDK/PSPMediaCenter/mod/modplayer.h
@@ -0,0 +1,31 @@
+// modplayer.h: headers for psp modplayer code
+//
+// All public functions for modplayer
+//
+//////////////////////////////////////////////////////////////////////
+#ifndef _MODPLAYER_GENERAL_H
+#define _MODPLAYER_GENERAL_H
+
+#include "../codec.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  Function prototypes for public functions
+    void MODsetStubs(codecStubs * stubs);
+
+//  Function prototypes for private functions
+    void Mod_Init(int channel);
+    int Mod_Play();
+    void Mod_Pause();
+    int Mod_Stop();
+    void Mod_End();
+    int Mod_Load(char *filename);
+    void Mod_Tick();
+    void Mod_Close();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
