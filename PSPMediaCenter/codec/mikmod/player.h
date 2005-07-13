7fe206b16a3d3811240ece69135e18d3e32702b1
adresd <adresd@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-07-13 11:03:40 +0000

    adding mikmod support

---
+++ PSPMediaCenter/codec/mikmod/player.h
@@ -0,0 +1,32 @@
+// player.h: headers for psp MIKMOD player code
+//
+// All public functions for MIKMOD player
+//
+//////////////////////////////////////////////////////////////////////
+#ifndef _MIKMODPLAYER_H_
+#define _MIKMODPLAYER_H_
+
+#include <mad.h>
+#include "../../codec.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  Function prototypes for public functions
+    void MIKMODsetStubs(codecStubs * stubs);
+
+//private functions
+    void MIKMOD_Init(int channel);
+    int MIKMOD_Play();
+    void MIKMOD_Pause();
+    int MIKMOD_Stop();
+    void MIKMOD_End();
+    int MIKMOD_Load(char *filename);
+
+    void MIKMOD_GetTimeString(char *dest);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
