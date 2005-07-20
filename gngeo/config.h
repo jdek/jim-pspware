fe62a1d057c794450d5250042e26f05bac7e55e5
lantus <lantus@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-07-20 00:53:59 +0000

    GNGEO : A NeoGeo emulator for PSP
    currently compiles and links but not tested yet, lots of debugging to come

---
+++ gngeo/config.h
@@ -0,0 +1,8 @@
+#include <pspiofilemgr.h>
+#include <pspmodulemgr.h>
+#include <pspsysmem.h>
+#include <pspthreadman.h>
+#include <psputils.h>
+
+#define DATA_DIRECTORY "fatms0:/PSP/GAME/pspgngeo/ROMS"
+#define VERSION "0.01"
