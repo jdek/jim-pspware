52f61e5b6b0dc430d43d0ceb8f20da1d263a865c
nevyn <nevyn@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-08-05 14:10:50 +0000

    Loads of new stuff: mikmodlib support, for sound (wav) and music (mod, xm, s3m, ...). Began making the library userdata-centric. The sound part is, but the rest needs updating. Look at my userdata code to see how it's done (really, I spent like five hours trying to figure userdatas out, so please make use of that knowledge :P). Added Lowser and an Applications dir of samples.

--- trunk/LuaPlayer/luaplayer.c
+++ trunk/LuaPlayer/luaplayer.c
@@ -6,6 +6,7 @@
 #include <pspctrl.h>
 
 #include "graphics.h"
+#include "sound.h"
 
 #include "lua.h"
 #include "lualib.h"
@@ -13,10 +14,23 @@
 
 #define MAX_IMAGES 1024
 
+#define true 1
+#define false 0
+
+#define SOUNDHANDLE "Sound"
+#define VOICEHANDLE "Voice"
+
 static lua_State *L;
 
 Image* images[MAX_IMAGES];
+
+
 static int currentImageHandle = 0;
+
+
+// predefines
+static Voice* pushVoice(lua_State *L);
+
 
 static int lua_ctrlRead(lua_State *L)
 {
@@ -552,6 +566,418 @@ static int lua_setCurrentDirectory(lua_State *L)
 	return 0;
 }
 
+
+
+// ===================================
+// Helper functions
+// ===================================
+
+
+void mystrcat(char * s, const char * b) {
+	while(*s != 0) s++;
+	while(*b != 0) *(s++) = *(b++);
+	*s = 0;
+}
+
+void stackDump (lua_State *L) {
+  int i;
+  int top = lua_gettop(L);
+  for (i = 1; i <= top; i++) {  /* repeat for each level */
+	int t = lua_type(L, i);
+	switch (t) {
+
+	  case LUA_TSTRING:  /* strings */
+		printf("`%s'", lua_tostring(L, i));
+		break;
+
+	  case LUA_TBOOLEAN:  /* booleans */
+		printf(lua_toboolean(L, i) ? "true" : "false");
+		break;
+
+	  case LUA_TNUMBER:  /* numbers */
+		printf("%g", lua_tonumber(L, i));
+		break;
+
+	  default:  /* other values */
+		printf("%s", lua_typename(L, t));
+		break;
+
+	}
+	printf("  ");  /* put a separator */
+  }
+  printf("\n");  /* end the listing */
+}
+
+// ===================================
+// Sound through mikmodlib and sound.*
+// ===================================
+
+
+// Music
+// ------------------------------
+
+static int lua_loadAndPlayMusicFile(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 && argc != 2) return luaL_error(L, "wrong number of arguments");
+	
+	const char *path = luaL_checkstring(L, 1);
+	BOOL loop = false;
+	if(argc == 2) loop = luaL_checknumber(L, 2);
+	
+	char fullpath[512];
+	getcwd(fullpath, 256);
+	mystrcat(fullpath, "/");
+	mystrcat(fullpath, path);
+	
+	loadAndPlayMusicFile(fullpath, loop);
+	
+	return 0;
+}
+
+static int lua_stopAndUnloadMusic(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 0 ) return luaL_error(L, "wrong number of arguments");
+	
+	stopAndUnloadMusic();
+	
+	return 0;
+}
+
+static int lua_musicIsPlaying(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 0 ) return luaL_error(L, "wrong number of arguments");
+	
+	BOOL result = musicIsPlaying();
+
+	lua_pushboolean(L, result);
+	return 1;
+}
+
+static int lua_setMusicVolume(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 ) return luaL_error(L, "wrong number of arguments");
+	int arg = luaL_checknumber(L, 1);
+
+	setMusicVolume(arg);
+
+	return 0;
+}
+
+
+static const luaL_reg Music_functions[] = {
+  {"playFile",          lua_loadAndPlayMusicFile},
+  {"stop",           	lua_stopAndUnloadMusic},
+  {"playing", 		lua_musicIsPlaying},
+  {"volume", 			lua_setMusicVolume},
+  {0, 0}
+};
+
+
+// Utility functions
+// ------------------------------
+
+static int lua_setSFXVolume(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 ) return luaL_error(L, "wrong number of arguments");
+	int arg = luaL_checknumber(L, 1);
+
+	setSFXVolume(arg);
+
+	return 0;
+}
+static int lua_setReverb(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 ) return luaL_error(L, "wrong number of arguments");
+	int arg = luaL_checknumber(L, 1);
+
+	setReverb(arg);
+
+	return 0;
+}
+static int lua_setPanSep(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 ) return luaL_error(L, "wrong number of arguments");
+	int arg = luaL_checknumber(L, 1);
+
+	setPanSep(arg);
+
+	return 0;
+}
+
+static const luaL_reg SoundSystem_functions[] = {
+  {"SFXVolume",          		lua_setSFXVolume},
+  {"reverb",           			lua_setReverb},
+  {"panoramicSeparation", 		lua_setPanSep},
+  {0, 0}
+};
+
+
+// The "Sound" userdata object.
+// ------------------------------
+// note: To implement a usedata object, see 
+// http://lua-users.org/wiki/UserDataExample
+
+static Sound** toSound (lua_State *L, int index)
+{
+  Sound** handle  = (Sound **)lua_touserdata(L, index);
+  if (handle == NULL) luaL_typerror(L, index, SOUNDHANDLE);
+  return handle;
+}
+
+
+
+static Sound** pushSound(lua_State *L) {
+	Sound ** sound = (Sound**)lua_newuserdata(L, sizeof(Sound*)); // stack:
+	luaL_getmetatable(L, SOUNDHANDLE); // sound, sound.meta
+	lua_pushvalue(L, -1); // sound, sound.meta, sound.meta
+	lua_setmetatable(L, -3); // sound, sound.meta
+	lua_pushstring(L, "__index");	//sound, sound.meta, __index
+	lua_pushstring(L, SOUNDHANDLE); // sound, sound.meta, __index, "Sound"
+	lua_gettable(L, LUA_GLOBALSINDEX);//sound, sound.meta, __index, Sound
+	lua_settable(L, -3); // sound.meta.__index = Sound
+					   // stack is sound, sound.meta
+	lua_pop(L, 1); // stack is sound
+	return sound;
+}
+
+static int Sound_new(lua_State *L) {
+	int argc = lua_gettop(L);
+	if(argc != 1 )
+		return luaL_error(L, "Need a file path argument as only argument.");
+	
+	// Load variables
+	char fullpath[512];
+	const char *path = luaL_checkstring(L, 1);
+	if(!path) return luaL_error(L, "Argument must be a file path.");
+	getcwd(fullpath, 256);
+	mystrcat(fullpath, "/");
+	mystrcat(fullpath, path);
+	
+	// Create the user object
+	Sound** newsound = pushSound(L);
+	*newsound = loadSound(fullpath);
+	
+	if (!*newsound) return luaL_error(L, "error loading sound");
+	
+	// Note: a userdata object has already been pushed.
+	return 1;
+}
+
+static int Sound_gc(lua_State *L) // garbage collect
+{
+	Sound** handle= toSound(L, 1);
+	unloadSound(*handle);
+	return 0;
+}
+
+static int Sound_tostring (lua_State *L)
+{
+  char buff[32];
+  sprintf(buff, "%p", *toSound(L, 1));
+  lua_pushfstring(L, "Sound (%s)", buff);
+  return 1;
+}
+
+
+static int Sound_play(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 ) return luaL_error(L, "Sound:play() takes no arguments. Also, call it with a colon, not a dot.");
+	
+	Sound** handle = toSound(L, 1);
+	
+	Voice *voice = pushVoice(L);
+	*voice = playSound(*handle);
+	
+	// the Voice is already pushed
+	return 1;
+}
+
+
+
+
+static const luaL_reg Sound_methods[] = {
+  {"load",          Sound_new},
+  {"new",           Sound_new},
+  {"play", 			Sound_play},
+  {0, 0}
+};
+static const luaL_reg Sound_meta[] = {
+  {"__gc",       Sound_gc},
+  {"__tostring", Sound_tostring},
+  {0, 0}
+};
+int Sound_register(lua_State *L) {
+	luaL_newmetatable(L, SOUNDHANDLE);
+	lua_pushstring(L, "__index");
+	lua_pushvalue(L, -2);
+	lua_settable(L, -3); // metatable.__index = metatable
+	
+	
+	luaL_openlib(L, 0, Sound_meta, 0);
+	luaL_openlib(L, SOUNDHANDLE, Sound_methods, 0);
+	
+	lua_pushstring(L, SOUNDHANDLE);
+	lua_gettable(L, LUA_GLOBALSINDEX);
+	luaL_getmetatable(L, SOUNDHANDLE);
+	lua_setmetatable(L, -2);
+	return 1;
+}
+
+
+// The "Voice" userdata object.
+// ------------------------------
+// note: To implement a usedata object, see 
+// http://lua-users.org/wiki/UserDataExample
+
+
+static Voice *toVoice (lua_State *L, int index)
+{
+  Voice* handle  = (Voice*)lua_touserdata(L, index);
+  if (handle == NULL) luaL_typerror(L, index, VOICEHANDLE);
+  return handle;
+}
+
+static Voice* pushVoice(lua_State *L) {
+	Voice * voice = (Voice*)lua_newuserdata(L, sizeof(Voice));
+	luaL_getmetatable(L, VOICEHANDLE);
+	lua_pushvalue(L, -1);
+	lua_setmetatable(L, -3);
+	
+	lua_pushstring(L, "__index");
+	lua_pushstring(L, VOICEHANDLE);
+	lua_gettable(L, LUA_GLOBALSINDEX);
+	lua_settable(L, -3); // voice.__index = Voice
+
+	lua_pop(L, 1);
+	return voice;
+}
+
+
+static int Voice_tostring (lua_State *L)
+{
+  char buff[32];
+  sprintf(buff, "%d", *toVoice(L, 1));
+  lua_pushfstring(L, "Voice (%s)", buff);
+  return 1;
+}
+
+
+static int Voice_stop(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 ) return luaL_error(L, "Voice:stop() takes no arguments. Also, call it with a colon, not a dot.");
+	
+	unsigned handle = *toVoice(L, 1);
+
+	stopSound(handle);
+
+	return 0;
+}
+
+static int Voice_setVolume(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 2 )
+		return luaL_error(L, "Voice:setVolume() takes one argument.");
+	
+	unsigned handle = *toVoice(L, 1);
+	int arg = luaL_checknumber(L, 2);
+
+	setVoiceVolume(handle, arg);
+
+	return 0;
+}
+
+static int Voice_setPanning(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 2 )
+		return luaL_error(L, "Voice:setPanning() takes one argument.");
+	
+	unsigned handle = *toVoice(L, 1);
+	int arg = luaL_checknumber(L, 2);
+	
+	setVoicePanning(handle, arg);
+
+	return 0;
+}
+
+static int Voice_setFrequency(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 2 )
+		return luaL_error(L, "Voice:setFrequency() takes one argument.");
+	
+	unsigned handle = *toVoice(L, 1);
+	int arg = luaL_checknumber(L, 2);
+
+	
+	setVoiceFrequency(handle, arg);
+
+	return 0;
+}
+
+static int Voice_playing(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	if(argc != 1 )
+		return luaL_error(L, "Voice:playing() takes no arguments. Also, call it with a colon, not a dot.");
+	
+	unsigned handle = *toVoice(L, 1);
+	
+	BOOL result = voiceIsPlaying(handle);
+
+	lua_pushboolean(L, result);
+	return 1;
+}
+
+
+
+static const luaL_reg Voice_methods[] = {
+  {"stop",          Voice_stop},
+  {"volume", 		Voice_setVolume},
+  {"pan", 			Voice_setPanning},
+  {"frequency", 	Voice_setFrequency},
+  {"playing", 	 	Voice_playing},
+  {0, 0}
+};
+static const luaL_reg Voice_meta[] = {
+  // Doesn't need to be gc'd, mikmod deallocates voice when it has stopped
+  {"__tostring", Voice_tostring},
+  {0, 0}
+};
+int Voice_register(lua_State *L) {
+	luaL_newmetatable(L, VOICEHANDLE);
+	lua_pushstring(L, "__index");
+	lua_pushvalue(L, -2);
+	lua_settable(L, -3); // metatable.__index = metatable
+	
+	
+	luaL_openlib(L, 0, Voice_meta, 0);
+	luaL_openlib(L, VOICEHANDLE, Voice_methods, 0);
+	
+	lua_pushstring(L, VOICEHANDLE);
+	lua_gettable(L, LUA_GLOBALSINDEX);
+	luaL_getmetatable(L, VOICEHANDLE);
+	lua_setmetatable(L, -2);
+	return 1;
+}
+
+
+// ===================================
+// LuaPlayer code
+// ===================================
+
+
+
 void runScript(const char* filename)
 {
 	L = lua_open();
@@ -601,6 +1027,12 @@ void runScript(const char* filename)
 	lua_register(L, "dir", lua_dir);
 	lua_register(L, "getCurrentDirectory", lua_getCurrentDirectory);
 	lua_register(L, "setCurrentDirectory", lua_setCurrentDirectory);
+	
+	//// Mikmod
+	luaL_openlib(L, "Music", Music_functions, 0);
+	luaL_openlib(L, "SoundSystem", SoundSystem_functions, 0);
+	Sound_register(L);
+	Voice_register(L);
 
 	int s = luaL_loadfile(L, filename);
 	if (s == 0) {
