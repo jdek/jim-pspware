93ebfb8213f44470cd0681c2a74d40d6cfb226cc
nevyn <nevyn@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-08-05 14:24:27 +0000

    added some files I missed in last commit

---
+++ trunk/LuaPlayer/Applications/Shine's Snake/index.lua
@@ -0,0 +1,264 @@
+--[[
+Snake, Copyright (c) 2005 Frank Buss <fb@frank-buss.de> (aka Shine)
+
+   artworks by Gundrosen
+   coding by Shine
+]]
+
+-- load images
+background = loadImage("background.png")
+tiles = loadImage("tiles.png")
+snake = createImage(480, 272)
+
+-- define tile positions
+tailR = { x = 0, y = 0 }
+tailT = { x = 1, y = 0 }
+tailL = { x = 2, y = 0 }
+tailB = { x = 3, y = 0 }
+
+headR = { x = 0, y = 1 }
+headT = { x = 1, y = 1 }
+headL = { x = 2, y = 1 }
+headB = { x = 3, y = 1 }
+
+bodyLT = { x = 0, y = 2 }
+bodyLB = { x = 1, y = 2 }
+bodyRT = { x = 2, y = 2 }
+bodyRB = { x = 3, y = 2 }
+bodyLR = { x = 4, y = 2 }
+bodyTB = { x = 5, y = 2 }
+
+apple = { x = 0, y = 3 }
+fly = { x = 1, y = 3 }
+
+-- define globals
+tails = {}
+heads = {}
+bodies = {}
+
+RIGHT = 0
+TOP = 1
+LEFT = 2
+BOTTOM = 3
+shouldExit = false
+
+tails[RIGHT] = tailR
+tails[TOP] = tailT
+tails[LEFT] = tailL
+tails[BOTTOM] = tailB
+
+heads[RIGHT] = headR
+heads[TOP] = headT
+heads[LEFT] = headL
+heads[BOTTOM] = headB
+
+bodies[RIGHT] = bodyLR
+bodies[TOP] = bodyTB
+bodies[LEFT] = bodyLR
+bodies[BOTTOM] = bodyTB
+
+score = 0
+high = 0
+
+target = {}
+targetImage = 0
+
+dx = 0
+dy = 0
+cellHead = {}
+cellTail = {}
+
+function createRandomTarget()
+	target.x = math.random(21)
+	target.y = math.random(15)
+	if math.random(2) == 1 then
+		targetImage = apple
+	else
+		targetImage = fly
+	end
+end
+
+function clearTileInSnakeImage(x, y)
+	fillRect(0, 16*x, 16*y, 16, 16, snake)
+end
+
+function drawTileToOffscreen(tile, x, y)
+	blitAlphaImageRect(16*tile.x, 16*tile.y, 16, 16, tiles, 16*x, 16*y);
+end
+
+function drawTileToSnakeImage(tile, x, y)
+	blitAlphaImageRect(16*tile.x, 16*tile.y, 16, 16, tiles, 16*x, 16*y, snake);
+end	
+
+function newGame()
+	clear(0, snake)
+	dx = 0
+	dy = 0
+	x = 12
+	y = 8
+	cellTail.x = x
+	cellTail.y = y
+	cellTail.direction = RIGHT
+	x = x+1
+	cellHead.x = x
+	cellHead.y = y
+	cellHead.direction = RIGHT
+	cellTail.next = cellHead
+	createRandomTarget()
+	drawTileToSnakeImage(tailR, cellTail.x, cellTail.y)
+	drawTileToSnakeImage(headR, cellHead.x, cellHead.y)
+
+	score = 0
+end
+
+function keyboardControl()
+	waitVblankStart();
+	pad = ctrlRead()
+	if isCtrlUp(pad) then
+		dx = 0
+		dy = -1
+	elseif isCtrlDown(pad) then
+		dx = 0
+		dy = 1
+	elseif isCtrlLeft(pad) then
+		dx = -1
+		dy = 0
+	elseif isCtrlRight(pad) then
+		dx = 1
+		dy = 0
+	elseif isCtrlStart(pad) then
+		shouldExit = true
+	elseif isCtrlCircle(pad) then
+		-- screenshot()
+	end
+end
+
+function move()
+	-- do nothing, if no movement vector defined
+	if dx == 0 and dy == 0 then
+		return
+	end
+	
+	-- save last head direction and position
+	lastX = cellHead.x
+	lastY = cellHead.y
+	lastDirection = cellHead.direction
+
+	-- add new head
+	cellHead.next = {}
+	cellHead = cellHead.next
+	cellHead.x = lastX + dx;
+	cellHead.y = lastY + dy;
+	
+	-- set direction for new head
+	direction = 0
+	if dx == 1 then
+		direction = RIGHT
+	elseif dy == -1 then
+		direction = TOP
+	elseif dx == -1 then
+		direction = LEFT
+	elseif dy == 1 then
+		direction = BOTTOM
+	end
+	cellHead.direction = direction
+	
+	-- check which body to draw at the place of the old head
+	clearTileInSnakeImage(lastX, lastY)
+	if lastDirection == RIGHT and direction == TOP then
+		drawTileToSnakeImage(bodyLT, lastX, lastY)
+	elseif lastDirection == TOP and direction == LEFT then
+		drawTileToSnakeImage(bodyLB, lastX, lastY)
+	elseif lastDirection == LEFT and direction == BOTTOM then
+		drawTileToSnakeImage(bodyRB, lastX, lastY)
+	elseif lastDirection == BOTTOM and direction == RIGHT then
+		drawTileToSnakeImage(bodyRT, lastX, lastY)
+	elseif lastDirection == RIGHT and direction == BOTTOM then
+		drawTileToSnakeImage(bodyLB, lastX, lastY)
+	elseif lastDirection == TOP and direction == RIGHT then
+		drawTileToSnakeImage(bodyRB, lastX, lastY)
+	elseif lastDirection == LEFT and direction == TOP then
+		drawTileToSnakeImage(bodyRT, lastX, lastY)
+	elseif lastDirection == BOTTOM and direction == LEFT then
+		drawTileToSnakeImage(bodyLT, lastX, lastY)
+	else 
+		drawTileToSnakeImage(bodies[direction], lastX, lastY)
+	end
+	
+	-- draw new head
+	drawTileToSnakeImage(heads[cellHead.direction], cellHead.x, cellHead.y)
+	
+	-- check for target
+	if cellHead.x == target.x and cellHead.y == target.y then
+		createRandomTarget()
+		score = score + 1
+	else
+		-- remove tail
+		clearTileInSnakeImage(cellTail.x, cellTail.y)
+		cellTail = cellTail.next
+
+		-- draw new tail image
+		if cellTail.direction ~= cellTail.next.direction then
+			cellTail.direction = cellTail.next.direction
+		end
+		clearTileInSnakeImage(cellTail.x, cellTail.y)
+		drawTileToSnakeImage(tails[cellTail.direction], cellTail.x, cellTail.y)
+	end
+end
+
+function isGameOver()
+	lastX = cellHead.x
+	lastY = cellHead.y
+	gameOver = false
+	if lastX >= 22 then
+		gameOver = true
+	end
+	if lastX < 1 then
+		gameOver = true
+	end
+	if lastY >= 16 then
+		gameOver = true
+	end
+	if lastY < 1 then
+		gameOver = true
+	end
+	cell = cellTail
+	while cell ~= cellHead do
+		if cell.x == lastX and cell.y == lastY then
+			gameOver = true
+			break
+		end
+		cell = cell.next
+	end
+	return gameOver
+end
+	
+-- init
+math.randomseed(os.time())
+newGame()
+
+-- game loop
+while true do
+	for i=0,4 do
+		keyboardControl()
+	end
+	move()
+	blitImage(0, 0, background)
+	blitAlphaImage(0, 0, snake)
+	drawTileToOffscreen(targetImage, target.x, target.y)
+	if score > high then
+		high = score
+	end
+	printDecimal(410, 81, score, 0);
+	printDecimal(429, 129, high, 0);
+	waitVblankStart()
+	flipScreen()
+	
+	if(shouldExit) then
+		break
+	end
+	if isGameOver() then
+		waitVblankStart(50)
+		newGame()
+	end
+end
---
+++ trunk/LuaPlayer/sound.c
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+#include <malloc.h>
+#include <psputils.h>
+#include "mikmod.h"
+
+#include "sound.h"
+
+#define MAX_MUSIC_CHAN 128
+#define MAX_SFX_CHAN 32
+static UNIMOD *musichandle = NULL;
+
+//Internals. Implement this somewhere else to get error reporting lua-style.
+void my_mikmod_error_handler(void)
+{
+	printf("LuaPlayer's Mikmod has a critical error:\n");
+	printf("_mm_critical %d\n", _mm_critical);
+	printf("_mm_errno %d\n", _mm_errno);
+	printf("%s\n", _mm_errmsg[_mm_errno]);
+	return;
+}
+extern void my_error_handler();
+
+// For internal Lua use
+
+void initMikmod() {
+	_mm_RegisterErrorHandler(my_mikmod_error_handler);
+	MikMod_RegisterAllLoaders();
+	MikMod_RegisterAllDrivers();
+	md_mode = DMODE_16BITS|DMODE_STEREO|DMODE_SOFT_SNDFX|DMODE_SOFT_MUSIC; 
+	md_reverb = 0;
+	md_pansep = 128;
+	md_volume = 128;
+	md_musicvolume = 96;
+	md_sndfxvolume = 128;
+	musichandle = NULL;
+	MikMod_Init();
+	MikMod_SetNumVoices(MAX_MUSIC_CHAN, MAX_SFX_CHAN);
+	Player_Start(0);
+}
+
+void unloadMikmod() {
+	if(musichandle) MikMod_FreeSong(musichandle);
+	Player_Stop();
+	MikMod_Exit();
+}
+
+// For external Lua use
+
+void loadAndPlayMusicFile(char* filename, BOOL loop) {
+	if(musichandle) stopAndUnloadMusic();
+	
+	musichandle = MikMod_LoadSong(filename, MAX_MUSIC_CHAN);
+	musichandle->loop = loop;
+	Player_Start(musichandle);
+}
+
+void stopAndUnloadMusic() {
+	Player_Stop();
+	MikMod_FreeSong(musichandle);
+	musichandle = NULL;
+}
+
+Sound* loadSound(char* filename) {
+	return WAV_LoadFN(filename);
+}
+
+void unloadSound(Sound* handle) {
+	WAV_Free(handle);
+}
+
+
+Voice playSound(Sound* handle) {
+	if(handle) { // Just for good measure, even if it's a precondition...
+		Voice voice = MikMod_PlaySample(handle,0,0);
+		setVoicePanning(voice, 127);
+		setVoiceVolume(voice, 255);
+		return voice;
+	}
+	return 0;
+}
+
+
+void stopSound(Voice handle) {
+	Voice_Stop(handle);
+}
+
+
+
+void setVoiceVolume(Voice handle, UWORD vol) {
+	if(vol>255) vol = 255;
+	Voice_SetVolume(handle, vol);
+}
+
+
+
+void setVoicePanning(Voice handle, ULONG pan) {
+	if(pan>255) pan = 255;
+	Voice_SetPanning(handle, pan);
+}
+
+
+void setVoiceFrequency(Voice handle, ULONG freq) {
+	if(freq>44000) freq = 44000;
+	Voice_SetFrequency(handle, freq);
+}
+
+extern BOOL musicIsPlaying() {
+	return Player_Active();
+}
+
+extern BOOL voiceIsPlaying(Voice handle) {
+	return Voice_Stopped(handle);
+}
+
+
+extern void setMusicVolume(unsigned arg) {
+	if(arg>128) arg = 128;
+	md_musicvolume = arg;
+}
+extern void setSFXVolume(unsigned arg) {
+	if(arg>128) arg = 128;
+	md_sndfxvolume = arg;
+}
+extern void setReverb(unsigned arg) {
+	if(arg>15) arg = 15;
+	md_reverb = arg;
+}
+extern void setPanSep(unsigned arg) {
+	if(arg>128) arg = 128;
+	md_pansep = arg;
+}
