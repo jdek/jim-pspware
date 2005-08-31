ef6c0f715c6f96bbcff36bef7d22d08af1a8f74e
adresd <adresd@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-08-31 21:09:47 +0000

    adding aslib target

---
+++ trunk/PSPMediaCenter/gui/aslib/libpspmc.h
@@ -0,0 +1,15 @@
+#ifndef _LIB_PSPMC_H_
+#define _LIB_PSPMC_H_
+
+/**
+ *  main routine 
+ */
+int libpspmc_init();
+int libpspmc_end();
+int libpspmc_stop();
+int libpspmc_loadplay(char *filepath);
+int libpspmc_geteos();
+int libpspmc_getinfo(unsigned char *string);
+int libpspmc_gettime(unsigned char *string);
+
+#endif
