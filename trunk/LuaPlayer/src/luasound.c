6af2a11ec68849dfb860dbb62d8f892ef326d5b9
shine <shine@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-09-09 08:19:09 +0000

    calling GC before allocating image, sound and music objects

--- trunk/LuaPlayer/src/luasound.c
+++ trunk/LuaPlayer/src/luasound.c
@@ -31,6 +31,7 @@ static int Music_loadAndPlay(lua_State *L)
 	strcat(fullpath, "/");
 	strcat(fullpath, path);
 	
+	luaC_collectgarbage(L);
 	loadAndPlayMusicFile(fullpath, loop);
 	
 	return 0;
@@ -161,6 +162,7 @@ static int Sound_load(lua_State *L) {
 	BOOL doloop = (argc==2)?lua_toboolean(L, 2):false;
 	
 	// Create the user object
+	luaC_collectgarbage(L);
 	Sound* newsound = loadSound(fullpath);;
 	if (!newsound) return luaL_error(L, "error loading sound");
 	Sound** luaNewsound = pushSound(L);
