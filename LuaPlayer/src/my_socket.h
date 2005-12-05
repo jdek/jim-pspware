abd6aa71336383b04d2473baeac4f7e44bd3ea37
shine <shine@ff2c0c17-07fa-0310-a4bd-d48831021cb5>
2005-12-05 23:09:49 +0000

    bugfixing and DNS resolving

--- LuaPlayer/src/my_socket.h
+++ LuaPlayer/src/my_socket.h
@@ -17,8 +17,8 @@ struct sockaddr_in {
         unsigned char sin_size; // size, not used
         unsigned char sin_family; // usually AF_INET
         unsigned short sin_port; // use htons()
-        unsigned char sin_addr[4];
-        char    sin_zero[8];
+        u32 sin_addr;
+        char sin_zero[8];
 };
 
 SOCKET sceNetInetSocket(int af, int type, int protocol);
